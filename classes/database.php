6d68d974521826cdef35ca82037cb80bc27d7775
米澤翔太郎 <yonezawashoutarou@yonezawashoutarounoMacBook-Air.local>
2021-04-02 18:06:25 +0900

    commit

--- classes/database.php
+++ classes/database.php
@@ -1,10 +1,10 @@
 <?php
 
 class Database {
-    private $server_name = "localhost";
-    private $username = "root";
-    private $password = "root"; 
-    private $db_name = "bleaf";
+    private $server_name = "us-cdbr-east-03.cleardb.com";
+    private $username = "bc7f2983a1e429";
+    private $password = "084bfab7"; 
+    private $db_name = "heroku_9a2ca3cb78f84b6";
 
     // --------------- Local ---------------
     // private $username = "root";
