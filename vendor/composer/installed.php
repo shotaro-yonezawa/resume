ab51cf93ab1443c41debb48e84bb399a254e51c5
米澤翔太郎 <yonezawashoutarou@yonezawashoutarounoMacBook-Air.local>
2021-04-08 17:56:34 +0900

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ef9b7cbc6dba489230f61fff41d0e143940e1a31',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ef9b7cbc6dba489230f61fff41d0e143940e1a31',
+    ),
+  ),
+);
